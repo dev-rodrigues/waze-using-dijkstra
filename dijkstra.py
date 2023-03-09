7bfbe14de9809f0b23555819d75c7a06d51aaebc
carlos henrique <carlos.hrq.rodrigues@gmail.com>
2023-03-09 20:55:28 -0300

    fix dijkstra to returns distance and best rout

--- dijkstra.py
+++ dijkstra.py
@@ -5,19 +5,28 @@ def dijkstra(grafo, inicio, fim):
     bairros = grafo.keys()
 
     distancias = {nome: float('inf') for nome in bairros}
-
     distancias[inicio.nome] = 0
+
+    antecessores = {nome: None for nome in bairros}
 
     heap = [(0, inicio.nome)]
 
     while heap:
         (distancia_atual, nome_atual) = heapq.heappop(heap)  # pega o nó com a menor distância
 
-        if nome_atual == fim.nome:  # se encontrou o destino, retorna a distância
-            return distancia_atual
+        if nome_atual == fim.nome:  # se encontrou o destino, retorna a distância e o melhor caminho
+            caminho_final = [fim.nome]
+
+            while antecessores[caminho_final[-1]] is not None:
+                caminho_final.append(antecessores[caminho_final[-1]])
+
+            caminho_final.reverse()
+
+            return distancia_atual, caminho_final
 
         if distancia_atual > distancias[nome_atual]:  # se a distância atual é maior que a distância registrada, ignora
             continue
+
         bairro_atual = grafo[nome_atual]  # pega o objeto Bairro correspondente ao nome
 
         for vizinho_nome in bairro_atual.obter_vizinhos():
@@ -29,7 +38,8 @@ def dijkstra(grafo, inicio, fim):
 
             if distancia < distancias[vizinho_nome]:  # se a nova distância é menor que a distância registrada, atualiza
                 distancias[vizinho_nome] = distancia
+                antecessores[vizinho_nome] = nome_atual
 
                 heapq.heappush(heap, (distancia, vizinho_nome))  # adiciona na lista de prioridades
 
-    return None
+    return None, None
--- main.py
+++ main.py
@@ -5,6 +5,10 @@ if __name__ == '__main__':
 
     bairros = get_bairros()
 
-    melhor_caminho = dijkstra(bairros, bairros["Ipanema"], bairros["Centro"])
+    (caminho, melhor_caminho) = dijkstra(
+        bairros,
+        bairros["Ipanema"],
+        bairros["Centro"]
+    )
 
-    print(f"A distância do Catete a Ipanema é de {melhor_caminho} km.")
+    print(f"A distância do Catete a Ipanema é de {caminho} km. E o melhor caminho é {melhor_caminho}")
